ad5b4fc3224f1c3cffdc13904bc6f0cd4215b194
alonp13 <alonp13@gmail.com>
2019-11-21 16:58:31 +0200

    Alon's version

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "myBank.h"
 
+void clear() {
+    while(getchar() != '\n');
+}
+
 int main()
 {
     char trans_type;
@@ -8,32 +12,55 @@ int main()
     {
         double amount,interest_rate;
         int account_number;
+        int check_amount,check_account;
         printf("Transaction type?: ");
         scanf(" %c", &trans_type);
         switch (trans_type)
         {
         case 'O':
             printf("Initial deposit?: ");
-            scanf(" %lf",&amount);
+            check_amount = scanf(" %lf",&amount);
+            if(check_amount != 1) {
+                printf("Wrong input!\n");
+                clear();
+                break;
+            }
             open_account(amount);
             break;
         case 'B':
             printf("Account number?: ");
-            scanf(" %d",&account_number);
+            check_account = scanf(" %d",&account_number);
+            if(check_account != 1) {
+                printf("Wrong input!\n");
+                clear();
+                break;
+            }
             check_balance(account_number);
             break;
         case 'D':
             printf("Account number?: ");
-            scanf(" %d",&account_number);
+            check_account = scanf(" %d",&account_number);
+            clear();
             printf("Amount?: ");
-            scanf(" %lf",&amount);
+            check_amount = scanf(" %lf",&amount);
+            if(check_account != 1 || check_amount != 1) {
+                printf("Wrong input!\n");
+                clear();
+                break;
+            }
             deposit(account_number,amount);
             break;
         case 'W':
             printf("Account number?: ");
-            scanf(" %d",&account_number);
+            check_account = scanf(" %d",&account_number);
+            clear();
             printf("Amount?: ");
-            scanf(" %lf",&amount);
+            check_amount = scanf(" %lf",&amount);
+            if(check_account != 1 || check_amount != 1) {
+                printf("Wrong input!\n");
+                clear();
+                break;
+            }
             withdraw(account_number,amount);
             break;
         case 'C':
